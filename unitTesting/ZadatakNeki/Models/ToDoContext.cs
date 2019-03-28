2c3bd91834da1435b1e0828d5f7d236b344a0cbf
mirrsan <46742702+mirrsan@users.noreply.github.com>
2019-03-28 11:31:09 +0100

    Add files via upload

---
+++ unitTesting/ZadatakNeki/Models/ToDoContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ZadatakNeki.Models
+{
+    public class ToDoContext : DbContext
+    {
+        
+        public ToDoContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<Osoba> Osobe { get; set; }
+        public virtual DbSet<Uredjaj> Uredjaji { get; set; }
+        public virtual DbSet<Kancelarija> Kancelarije { get; set; }
+        public virtual DbSet<OsobaUredjaj> OsobaUredjaj { get; set; }
+    }
+}
